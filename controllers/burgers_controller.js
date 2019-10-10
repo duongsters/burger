5ae87cf5165369ecc332f383fb4b0b0e9bd9f92e
Andrew Duong <duongsters@gmail.com>
2019-10-10 09:44:10 -0700

    created Export routes for server.js to use

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,3 +6,14 @@ var routeer = express.Router();
 // Import the model (burger.js) to use its database functions
 var burger = require("../models/burger.js");
 
+
+// Create all our routes and set up logic within those routes where required.
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
