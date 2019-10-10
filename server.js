37c219e284471aa7ce609783d4701ba164b9b54d
Andrew Duong <duongsters@gmail.com>
2019-10-10 09:25:12 -0700

    initialized listening port

--- server.js
+++ server.js
@@ -25,3 +25,13 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them
 var burgerRoutes = require("./controllers/burgers_controller.js");
+app.use(burgerRoutes);
+
+
+
+//--------------Listenng PORT---------------------------------//
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
