90614038dec3996bc7d9f6818b62912d2f7fd2cd
Andrew Duong <duongsters@gmail.com>
2019-10-10 09:10:54 -0700

    added more burger sample values into seeds.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,17 @@
 INSERT INTO burgers (burger_name, devoured) 
-VALUES ('Double Cheeese Burger', true);
+VALUES ('Double Cheese Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Regular Cheeseburger', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Spicy Chicken Sandwich', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Chicken Sandwich', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Veggie Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Rice Burger', false);
