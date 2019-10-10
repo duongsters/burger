a84e30e3b18681485b7cd3a4c3a755bab68b7ea9
Andrew Duong <duongsters@gmail.com>
2019-10-10 14:35:45 -0700

    added .env file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,29 @@
 // -----Set up MySQL connection-----
 var mysql = require('mysql');
 
+//connection to burger_db database of sql workbench
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
 
+    })
+}
 
 //connection to burger_db database of sql workbench
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+// });
 
 
 
