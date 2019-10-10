7227fbeb9c211217ad9f327dc86447173305e83d
Andrew Duong <duongsters@gmail.com>
2019-10-10 15:23:49 -0700

    installed express handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 //--express connection
 var express = require("express");
 
-var routeer = express.Router();
+var router = express.Router();
 
 // Import the model (burger.js) to use its database functions
 var burger = require("../models/burger.js");
