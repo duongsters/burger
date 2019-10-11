fd4435612c2f0aac490ec8f50906dd800b887a3b
Andrew Duong <duongsters@gmail.com>
2019-10-10 23:41:51 -0700

    created onclick call reading on the submit button on adding a burger to the list

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,20 +1,44 @@
-$(function(){
-    $(".change-devoured").on("click", function(event){
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".change-devoured").on("click", function (event) {
         var id = $(this).data("id");
         var newDevoured = $(this).data("newdevoured");
 
         var newDevouredState = {
             devoured: newDevoured
         };
-
+        // Send the html PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevouredState
         })
+            .then(
+                function () {
+                    console.log("changed devoured to", newDevoured);
+                    location.reload();
+                }
+            );
+    });
+
+    // onclick call reading on the submit button on adding a burger to the list
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#ca").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        })
         .then(
             function() {
-                console.log("changed devoured to", newDevoured);
+                console.log("created new burger");
                 location.reload();
             }
         );
     });
+
+});
