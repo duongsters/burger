8388e6aebac90028268047129719f5ae5e2e5c6e
Andrew Duong <duongsters@gmail.com>
2019-10-11 00:09:22 -0700

    mysql typo syntax error from CLI...forgot to define 'condition' variable that would be used within the .delete express call method function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ var burger = require("../models/burger.js");
 
 
 // Created all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
     burger.selectAll(function (data) {
         var hBarObject = {
             burgers: data
@@ -19,7 +19,7 @@ router.get("/", function (req, res) {
 });
 
 //.post express call method in posting new burgers to the db
-router.post("/api/burgers", function (req, res) {
+router.post("/api/burgers", function(req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ],
@@ -50,6 +50,8 @@ router.put("/api/burgers/:id", function (req, res) {
 
 //.delete express call method in deleting a burger from the db
 router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
     burger.delete(condition, function (result) {
         if (result.affectedRows == 0) {
             return res.status(404).end();
