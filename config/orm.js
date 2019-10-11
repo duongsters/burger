879c5e15051faf1ba2c0720e5e776ad7a58afd2e
Andrew Duong <duongsters@gmail.com>
2019-10-10 21:48:46 -0700

    created 'objToSql', a helper function, that runs to convert object key/value pairs to SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,7 @@
 var connection = require("../config/connection.js");
 
 // helper function for SQL syntax
+//... the helper function loops through and creates an array of question marks - and turns it into a string.
 function printQuestionMarks(num) {
     var arr = [];
     for(var j=0; j<num; j++) {
@@ -9,3 +10,23 @@ function printQuestionMarks(num) {
     }
     return arr.toString();
 }
+
+// 'objToSql', a helper function, that runs to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  // loop through the keys and push the key/value as a string int arr
+    for(var key in ob) {
+        var value = ob[key];
+            // check to skip hidden properties
+        if(Object.hasOwnPropert.call(ob, key)) {
+                  // if string with spaces, then add quotations 
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "'" + value);
+        }
+    }
+      // returns array of strings to a single comma-separated string
+    return arr.toString();
+}
+
