06d34ef7f6c623cb740c9e95e0ee8f534f65ead7
Andrew Duong <duongsters@gmail.com>
2019-10-10 21:58:56 -0700

    created 'insertOne' callback function runs by inserting a new burger to the 'burgers' table

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ var connection = require("../config/connection.js");
 //... the helper function loops through and creates an array of question marks - and turns it into a string.
 function printQuestionMarks(num) {
     var arr = [];
-    for(var j=0; j<num; j++) {
+    for (var j = 0; j < num; j++) {
         arr.push("?");
     }
     return arr.toString();
@@ -14,19 +14,56 @@ function printQuestionMarks(num) {
 // 'objToSql', a helper function, that runs to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
-  // loop through the keys and push the key/value as a string int arr
-    for(var key in ob) {
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
         var value = ob[key];
-            // check to skip hidden properties
-        if(Object.hasOwnPropert.call(ob, key)) {
-                  // if string with spaces, then add quotations 
+        // check to skip hidden properties
+        if (Object.hasOwnPropert.call(ob, key)) {
+            // if string with spaces, then add quotations 
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
             arr.push(key + "'" + value);
         }
     }
-      // returns array of strings to a single comma-separated string
+    // returns array of strings to a single comma-separated string
     return arr.toString();
 }
 
+
+// Object for all our SQL statement functions.
+var orm = {
+    // callback function 'selectAll' runs by takes in all values from tableInput from burgers_db database and selects all values 'burgers' table
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw errl
+            }
+            cb(result);
+        });
+    },
+
+    // 'insertOne' callback function runs by inserting a new burger to the 'burgers' table
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
+    },
+    
+}
