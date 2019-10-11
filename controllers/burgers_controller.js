71fd2ae674a22cdd6b324b07be977ab5a26fcbae
Andrew Duong <duongsters@gmail.com>
2019-10-10 17:47:46 -0700

    .put express call method in updating the burger db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -48,6 +48,17 @@ router.put("/api/burgers/:id", function(req, res){
     });
 });
 
+//.delete express call method in deleting a burger from the db
+routers.delete("/api/burgers/:id", function(req,res){
+    burger.delete(condition, function(result){
+        if(result.affectedRows == 0){
+            return res.status(404).end();
+        }
+        else{
+            res.status(200).end();
+        }
+    });
+});
 
 
 
