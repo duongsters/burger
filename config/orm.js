fb210c53a0a6efdeaf3d378c05043254994a9518
Andrew Duong <duongsters@gmail.com>
2019-10-10 21:40:36 -0700

    created helper function for SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,11 @@
+// imports MySQL connection call set within variable 'connection.js' file
 var connection = require("../config/connection.js");
+
+// helper function for SQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+    for(var j=0; j<num; j++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
